48f1f929a650cb329f0b475c94c0e6c9628e9e82
yangkainn <yangkainn@gmail.com>
2025-05-30 10:52:24 +0800

    convertWebP

--- blocksuite/affine/components/src/resource/resource.ts
+++ blocksuite/affine/components/src/resource/resource.ts
@@ -7,7 +7,7 @@ import {
   signal,
 } from '@preact/signals-core';
 import type { TemplateResult } from 'lit-html';
-import { convertToWebP } from '@../../../blocks/image/src/utils';
+import { convertToWebP } from '../../../blocks/image/src/utils';
 
 export type ResourceKind = 'Blob' | 'File' | 'Image';
 
